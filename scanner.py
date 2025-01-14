3162dc1185aef6af0143793c9549fea618e30ac5
Manas <manasadutiya@gmail.com>
2025-01-15 01:03:26 +0530

    third commit

--- scanner.py
+++ scanner.py
@@ -57,5 +57,35 @@ class WebSecScanner:
         all explore pages of a website while staying within the specified domain.
         """
         
-        
+    def check_sql_injec(self, url: str) -> None:
+        """Test for potential SQL injection vulnerabilities"""
+        sql_payloads = ["'","1' OR '1' = '1", "' OR 1=1--","' UNION SELECT NULL--"]
+
+        for payload in sql_payloads:
+            try:
+                parsed = urllib.parse.urlparse(url)
+                params = urllib.parse.parse_qs(parsed.query)
+
+                for param in params:
+                    test_url = url.replace(
+                        f"{param} = {params[param][0]}",
+                        f"{param} = {payload}"
+                    )
+                    response = self.session.get(test_url)
+
+                    if any(error in response.text.lower() for error in ['sql','mysql','sqlite','postgresql','oracle']):
+                        self.report_vulnerability({
+                            'type':'SQL Injection',
+                            'url' : url,
+                            'parameter':param,
+                            'payload':payload
+                        })
+            except Exception as e:
+                print(f"Error testing SQL injection on {url}: {str(e)}")
+        """This function performs sql injection checks by testing URL against
+        common SQL injection payloads and looking for error messages that might
+        hint a security vulnerability"""
+
+    
+
         
